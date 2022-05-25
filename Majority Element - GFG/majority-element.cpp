5eebecd639c74af321171e2f253eb549484b036d
Abhinav Anand <manasgmishra@gmail.com>
2022-05-25 11:07:56 +0530

    Added solution - LeetHub

--- Majority Element - GFG/majority-element.cpp
+++ Majority Element - GFG/majority-element.cpp
@@ -13,21 +13,48 @@ class Solution{
      // Function to find majority element in the array
     // a: input array
     // size: size of input array
+    /* Function to find the candidate for Majority */
+    
+    // Using Moore's voting algorithm
+int findCandidate(int a[], int size)
+{
+    int maj_index = 0, count = 1;
+    for (int i = 1; i < size; i++) {
+        if (a[maj_index] == a[i])
+            count++;
+        else
+            count--;
+        if (count == 0) {
+            maj_index = i;
+            count = 1;
+        }
+    }
+    return a[maj_index];
+}
+
+/* Function to check if the candidate
+   occurs more than n/2 times */
+bool isMajority(int a[], int size, int cand)
+{
+    int count = 0;
+    for (int i = 0; i < size; i++)
+
+        if (a[i] == cand)
+            count++;
+
+    if (count > size / 2)
+        return 1;
+
+    else
+        return 0;
+}
     int majorityElement(int a[], int size)
     {
-        // TC = O(N) SC = O(N)
-        map<int, int> m;
-        for(int i = 0; i < size; i++)
-        {
-            m[a[i]]++;
-        }
-        for(auto i : m)
-        {
-            if(i.second > size / 2)
-            {
-                return i.first;
-            }
-        }
+        // TC = O(N) SC = O(1)
+        int ans = findCandidate(a, size);
+        if(isMajority(a, size, ans))
+        return ans;
+        else
         return -1;
     }
 };
