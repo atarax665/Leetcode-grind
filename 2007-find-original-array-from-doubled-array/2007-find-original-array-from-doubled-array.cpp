eb393fa6fbbc5497c7f762428342da29b17bcbec
Abhinav Anand <manasgmishra@gmail.com>
2022-09-15 11:18:22 +0530

    Time: 541 ms (76.90%), Space: 116.7 MB (98.35%) - LeetHub

--- 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
+++ 2007-find-original-array-from-doubled-array/2007-find-original-array-from-doubled-array.cpp
@@ -1,25 +1,87 @@
 class Solution {
 public:
-    vector<int> findOriginalArray(vector<int>& changed) {
-        map<int, int> m;
-        vector<int> ans;
-        int n = changed.size();
-        sort(changed.begin(), changed.end());
-        if(n % 2 == 1)
+    int search(vector<int>& arr, int low, int high, vector<bool>& vis, int x)
+    {
+        int ans = -1;
+        
+        while(low <= high)
         {
-            return {};
+            int mid = low + (high - low) / 2;
+            
+            if(arr[mid] == x && vis[mid] == false)
+            {
+                ans = mid;
+                
+                high = mid - 1;
+            }
+            else if(arr[mid] == x && vis[mid] == true)
+            {
+                low = mid + 1;
+            }
+            else if(arr[mid] < x)
+            {
+                low = mid + 1;
+            }
+            else if(arr[mid] > x)
+            {
+                high = mid - 1;
+            }
         }
-        for(auto i : changed)
-        {
-            m[i]++;
-        }
-        for(int i = 0; i < n; i++) {
-          if (m[changed[i]] == 0) continue;
-          if (m[changed[i] * 2] == 0) return {};
-          ans.push_back(changed[i]);
-          m[changed[i]]--;
-          m[changed[i] * 2]--;
-        }
+        
         return ans;
+    }
+    
+    vector<int> findOriginalArray(vector<int>& arr) {
+        
+        int n = arr.size();
+        
+        // if size of arr is odd then no res. possible
+        
+        if(n % 2)
+            return {};
+        
+        // sort the array
+        
+        sort(arr.begin(), arr.end());
+        
+        // declare a vis array, initialize with false
+        
+        vector<bool> vis(n, false);
+        
+        // res will store the elements of original array
+        
+        vector<int> res;
+        
+        for(int i = 0; i < n; i++)
+        {
+            if(vis[i] == false)
+            {
+                // find the index of doubled element in arr
+                
+                int idx = search(arr, i + 1, n - 1, vis, 2 * arr[i]);
+                
+                // if double element is found, then mark single and double element as found
+                
+                // push the single element into res
+                
+                if(idx != -1)
+                {
+                    vis[i] = true;
+                
+                    vis[idx] = true;
+                
+                    res.push_back(arr[i]);
+                }
+                
+                // if double element is not found in array, then return empty array
+                
+                else if(idx == -1)
+                {
+                    return {};
+                }
+            }
+        }
+        
+        return res;
     }
 };
