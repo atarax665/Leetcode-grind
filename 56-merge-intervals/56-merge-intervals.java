d5fdcac76c07ef686f96e6a12f9c0a245a4dc06b
Abhinav Anand <manasgmishra@gmail.com>
2022-09-17 17:21:04 +0530

    Time: 17 ms (36.05%), Space: 54.6 MB (83.07%) - LeetHub

--- 56-merge-intervals/56-merge-intervals.java
+++ 56-merge-intervals/56-merge-intervals.java
@@ -1,24 +1,35 @@
 class Solution {
         public int[][] merge(int[][] intervals) {
+        // First, sort the intervals based on their start time
+        // The one with smaller start time comes first (ascending)
+        Arrays.sort(intervals, (a,b) -> (a[0] - b[0]));
         
-        List <int[]> ans = new ArrayList<>();
-        
-        Arrays.sort(intervals, (a,b) -> a[0]-b[0]);
-        
-        int start = intervals[0][0];
-        int end = intervals[0][1];
-        
-        for(int i = 0; i < intervals.length; i++){
-            if(intervals[i][0]<=end){
-                end = Math.max(end,intervals[i][1]);
+        ArrayList<int []> result = new ArrayList<>();
+        // Now we have a sorted interval list, so go through all of them and combine
+        for (int i = 0; i < intervals.length; i++) {
+            // Make sure we have at least one event in result to be compared with
+            if (i == 0) {
+                result.add(intervals[0]); 
+                continue;
             }
-            else{
-                ans.add(new int[]{start, end});
-                start = intervals[i][0];
-                end = intervals[i][1];
+            
+            int[] prev = result.get(result.size()-1);
+            int[] curr = intervals[i];
+            
+            // intervals overlap when:
+            // prev ends after/when curr starts
+            if (prev[1] >= curr[0]) {
+                // The new interval should cover both events
+                // So it has an smallest start time and largest end time
+                // Since it's the interval in result that overlaps with our new
+                // interval, we just modify that interval in result
+                // (it's guarantee that start time of prev is smaller or equal
+                // to that of curr since we traverse a sorted array)
+                prev[1] = Math.max(prev[1], curr[1]);
+            } else {
+                result.add(curr);
             }
         }
-        ans.add(new int[]{start, end});
-        return ans.toArray(new int[0][]);
+        return result.toArray(new int[result.size()][2]);
     }
 }
