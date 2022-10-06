56d8ed04605dd496ac761253be96b1b3d2d631a9
Abhinav Anand <manasgmishra@gmail.com>
2022-10-07 01:29:27 +0530

    Time: 400 ms (31.63%), Space: 222 MB (20.21%) - LeetHub

--- 981-time-based-key-value-store/981-time-based-key-value-store.java
+++ 981-time-based-key-value-store/981-time-based-key-value-store.java
@@ -1,34 +1,37 @@
 class TimeMap {
-    
-    // HashMap of key(String) corresponding of TreeMap<TimeStamp,Value>>
-    HashMap<String,TreeMap<Integer,String>> hm;
-    
+  public Map<String,TreeMap<Integer,String>> hm= new HashMap<>();
     public TimeMap() {
-        hm = new HashMap<String,TreeMap<Integer,String>>();
+    
     }
     
     public void set(String key, String value, int timestamp) {
-
-        TreeMap<Integer,String> tmap;
-        if(hm.get(key) != null)
-            tmap = hm.get(key);  
-        else
-            tmap = new TreeMap<>(Collections.reverseOrder());  
-        tmap.put(timestamp,value);
-        hm.put(key,tmap);
+        if(!hm.containsKey(key))
+       {
+           hm.put(key,new TreeMap<>(Collections.reverseOrder()));
+           hm.get(key).put(timestamp,value);
+       }
+           
+            hm.get(key).put(timestamp,value);
         
+       
+
+    
     }
     
     public String get(String key, int timestamp) {
         for(Map.Entry m : hm.entrySet()){
+            
             if(m.getKey().equals(key)){
-                TreeMap<Integer,String> tmap = (TreeMap<Integer,String>)m.getValue();
-                for(Map.Entry tm : tmap.entrySet()){
-                    if((int)tm.getKey() <= timestamp) return String.valueOf(tm.getValue());
+               
+                TreeMap<Integer,String> tmap = (    TreeMap<Integer,String>)m.getValue();
+               
+                for(Map.Entry i : tmap.entrySet()){
+                    if((int)i.getKey() <= timestamp) 
+                    return String.valueOf(i.getValue());
                 }
             }
         }
+       
         return "";
-        
     }
 }
