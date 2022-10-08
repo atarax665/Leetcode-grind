70c5fc3acd2ec062cc0528db3bd1250a70b2a858
Abhinav Anand <manasgmishra@gmail.com>
2022-10-08 13:17:47 +0530

    Time: 85 ms (30.34%), Space: 32.3 MB (28.18%) - LeetHub

---
+++ 365-water-and-jug-problem/365-water-and-jug-problem.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    bool dfs(int x, int y, int z, int curr, vector<int> &vis)
+    {
+        if (curr < 0 || curr > x + y || vis[curr] == 1)
+            return false;
+        if (curr == z)
+            return true;
+        vis[curr] = 1;
+        bool a = dfs(x, y, z, curr + x, vis);
+        bool b = dfs(x, y, z, curr - x, vis);
+        bool c = dfs(x, y, z, curr + y, vis);
+        bool d = dfs(x, y, z, curr - y, vis);
+        return a || b || c || d;
+    }
+    bool canMeasureWater(int x, int y, int z)
+    {
+        if (x + y == z || x == z || y == z)
+            return true;
+        if (x + y < z)
+            return false;
+        int m = x + y; // max capacity
+        vector<int> vis(m + 1, 0);
+        return dfs(x, y, z, 0, vis);
+    }
+};
