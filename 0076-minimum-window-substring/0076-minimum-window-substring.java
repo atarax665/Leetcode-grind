e67e91f182784c9469bae52d2ed1015bff8e7f91
Abhinav Anand <manasgmishra@gmail.com>
2022-10-23 23:03:22 +0530

    Time: 6 ms (89.68%), Space: 44.1 MB (66.03%) - LeetHub

---
+++ 0076-minimum-window-substring/0076-minimum-window-substring.java
@@ -0,0 +1,52 @@
+class Solution {
+    public String minWindow(String s, String t) {
+        
+        if (t.length () > s.length ())
+        {
+            return "";
+        }
+        
+        int i = 0, j = 0, start = -1, end = -1, length = 0, minLength = Integer.MAX_VALUE;
+        int[] frequencyS = new int[128];
+        int[] frequencyT = new int[128];
+        
+        for (char c : t.toCharArray ())
+        {
+            frequencyT[c]++;
+            if (frequencyT[c] == 1)
+            {
+                length++;
+            }
+        }
+        
+        while (i < s.length())
+        {
+            char c = s.charAt (i++);
+            ++frequencyS[c];
+            if (frequencyS[c] == frequencyT[c])
+            {
+                length--;
+            }
+            
+            while (length == 0)
+            {
+                if (i - j < minLength)
+                {
+                    start = j;
+                    end = i;
+                    minLength = i - j;
+                }
+                
+                char ch = s.charAt (j++);
+                if (frequencyS[ch] == frequencyT[ch])
+                {
+                    length++;
+                }
+                
+                frequencyS[ch]--;
+            }
+        }
+        
+        return minLength == Integer.MAX_VALUE ? "" : s.substring (start, end);
+    }
+}
