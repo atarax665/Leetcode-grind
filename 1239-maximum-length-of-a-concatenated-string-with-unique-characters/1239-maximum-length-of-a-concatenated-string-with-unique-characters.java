671edb11fad10f5af1fd7014730d979e7bb3055f
Abhinav Anand <manasgmishra@gmail.com>
2022-10-25 01:09:42 +0530

    Time: 22 ms (75.26%), Space: 48.9 MB (63.16%) - LeetHub

--- 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
+++ 1239-maximum-length-of-a-concatenated-string-with-unique-characters/1239-maximum-length-of-a-concatenated-string-with-unique-characters.java
@@ -9,12 +9,12 @@ class Solution {
         if(max<current.length())
             max =current.length();
         for(int i=start; i<arr.size();i++){
-            if(!isValid(current,arr.get(i))) continue;
+            if(!check(current,arr.get(i))) continue;
             backTrack(arr,current+arr.get(i),i+1);
         }
     }
     
-    private boolean isValid(String currentString, String newString){
+    private boolean check(String currentString, String newString){
         boolean[] array = new boolean[26];
         for (int i = 0; i < newString.length(); i++) {
             char c = newString.charAt(i);
