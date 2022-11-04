27fce5c9ff78c0c319cf1762cbaa3b1f7be19ab5
Abhinav Anand <manasgmishra@gmail.com>
2022-11-04 15:31:21 +0530

    Time: 5 ms (40.98%), Space: 54.1 MB (57.41%) - LeetHub

--- 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.java
@@ -1,65 +1,34 @@
 class Solution {
     public int[] findBall(int[][] grid) {
-     int[] curr= new int[grid[0].length];
+        int[] curr= new int[grid[0].length];
      int[] ans= new int[grid[0].length];
-        
-    if(grid[0].length == 1)
-    {
-        int[] a = {-1};
-        return a;
-    }
-     // Arrays.fill(ans,-1);
+     Arrays.fill(ans,-1);
      for(int j=0;j<grid[0].length;j++)
      {
     curr[j]=j;
      }
-        
-     for(int i=0;i<grid.length;i++)
+     for(int i=0;i<grid.length;i++){
+     for(int j=0;j<grid[i].length-1;j++)
      {
-     for(int j=0;j<grid[i].length;j++)
+    if(grid[i][j]==1&&grid[i][j+1]==1)
+    ans[j + 1]=curr[j];
+    if(grid[i][j]==-1&& grid[i][j+1]==-1)
+    ans[j] = curr[j + 1];
+     } 
+        curr = ans.clone();
+         if(i == grid.length - 1)
+             break;
+         Arrays.fill(ans, -1);
+    }
+        int[] finalAns = new int[grid[0].length];
+        Arrays.fill(finalAns, -1);
+        for(int j=0;j<grid[0].length;j++)
      {
-         if(ans[j]==-1)
-         {
-             continue;
-         }
-         if(i == 0 && ((j == 0 && grid[i][curr[j]] == -1) || (j == grid[0].length - 1 && grid[i][curr[j]] == 1)))
-         {
-             curr[j] = -1;
-         }
-         else if(curr[j]!=grid[i].length-1 && curr[j] != 0 && ((grid[i][curr[j]] == 1 && grid[i][curr[j] + 1] == -1) || (grid[i][curr[j]] == -1 && grid[i][curr[j] - 1] == 1)))
-         {
-             curr[j] = -1;
-         }
-         else if(curr[j]!=grid[i].length-1 && curr[j] != 0 && grid[i][curr[j]] == -1)
-         {
-             if(grid[i][curr[j] - 1] == 1)
-                 curr[j] = -1;
-             else
-                 curr[j] = curr[j] - 1;
-         }
-         
-        else if(curr[j]!=grid[i].length-1 && grid[i][curr[j]]==1&&grid[i][curr[j]+1]==1)
-        {
-            if(curr[j] + 1 > grid[i].length - 1)
-                curr[j] = -1;
-            else
-                curr[j] = curr[j] + 1;
-        }
-            
-        else if(curr[j] != 0 && grid[i][curr[j]] == -1 && grid[i][curr[j]-1]==-1 )
-        {
-            if(curr[j] - 1 < 0)
-                curr[j] = -1;
-            else
-                curr[j] = curr[j] - 1;
-        }
-         else
-         {
-             curr[j] = -1;
-         }
+    if(ans[j] != -1)
+    {
+        finalAns[ans[j]] = j;
+    }
      }
-         ans = curr.clone();
-    }   
-    return ans;
+    return finalAns;
 }
 }
